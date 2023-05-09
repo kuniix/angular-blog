546e08b6835c926b4bc4c15be053b3dd1bf86203
Lucas N <lucasn.norato@gmail.com>
2023-05-09 19:29:06 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,17 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "photo":"aaa",
+        "title":"Novo filme do Hulk",
+        "description":"Veja o novo filme do hulk",
+        "photoCover": "https://observatoriodocinema.uol.com.br/wp-content/uploads/2022/08/O-Incrivel-Hulk-Divulgacao-1200x900-1.jpg"
+    },
+    {
+        "id":"2",
+        "photo":"bbbb",
+        "title":"Novos episodios de HxH",
+        "description":"é verdade",
+        "photoCover": "https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/05/Killua-Featured.jpg"
+    }
+
+]
